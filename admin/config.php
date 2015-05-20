69a54321bd92903281d2065577cdc320b9e34530
grphm-robot <robot@grapheme.ru>
2015-05-20 16:34:08 +0300

    init commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://new.tetradirychki.com/admin/');
+define('HTTP_CATALOG', 'http://new.tetradirychki.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://new.tetradirychki.com/admin/');
+define('HTTPS_CATALOG', 'http://new.tetradirychki.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/rikardo/data/www/new.tetradirychki.com/admin/');
+define('DIR_SYSTEM', '/var/www/rikardo/data/www/new.tetradirychki.com/system/');
+define('DIR_DATABASE', '/var/www/rikardo/data/www/new.tetradirychki.com/system/database/');
+define('DIR_LANGUAGE', '/var/www/rikardo/data/www/new.tetradirychki.com/admin/language/');
+define('DIR_TEMPLATE', '/var/www/rikardo/data/www/new.tetradirychki.com/admin/view/template/');
+define('DIR_CONFIG', '/var/www/rikardo/data/www/new.tetradirychki.com/system/config/');
+define('DIR_IMAGE', '/var/www/rikardo/data/www/new.tetradirychki.com/image/');
+define('DIR_CACHE', '/var/www/rikardo/data/www/new.tetradirychki.com/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/rikardo/data/www/new.tetradirychki.com/download/');
+define('DIR_LOGS', '/var/www/rikardo/data/www/new.tetradirychki.com/system/logs/');
+define('DIR_CATALOG', '/var/www/rikardo/data/www/new.tetradirychki.com/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'k.,jdmythtdyetnyfghfcyj');
+define('DB_DATABASE', 'ocstore_tetradirychki');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://new.tetradirychki.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://new.tetradirychki.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/rikardo/data/www/new.tetradirychki.com/catalog/');
+define('DIR_SYSTEM', '/var/www/rikardo/data/www/new.tetradirychki.com/system/');
+define('DIR_DATABASE', '/var/www/rikardo/data/www/new.tetradirychki.com/system/database/');
+define('DIR_LANGUAGE', '/var/www/rikardo/data/www/new.tetradirychki.com/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/rikardo/data/www/new.tetradirychki.com/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/rikardo/data/www/new.tetradirychki.com/system/config/');
+define('DIR_IMAGE', '/var/www/rikardo/data/www/new.tetradirychki.com/image/');
+define('DIR_CACHE', '/var/www/rikardo/data/www/new.tetradirychki.com/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/rikardo/data/www/new.tetradirychki.com/download/');
+define('DIR_LOGS', '/var/www/rikardo/data/www/new.tetradirychki.com/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'k.,jdmythtdyetnyfghfcyj');
+define('DB_DATABASE', 'ocstore_tetradirychki');
+define('DB_PREFIX', 'oc_');
+?>
